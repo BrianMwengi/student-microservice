a416c2be3d1c83fbb4e8420ecf142e319ace21f7
Brian Mwengi <mwengibrian@gmail.com>
2024-09-19 12:13:53 +0300

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stream;
+use App\Models\Student;
+use App\Models\ClassForm;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('students', [
+            //
+        ]);
+    }
 }
