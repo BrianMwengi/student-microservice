3f59ad47d153dd7880ec3a8b53f4915af62924e4
Brian Mwengi <mwengibrian@gmail.com>
2024-09-19 11:33:06 +0300

    Update students and streams tables

--- database/migrations/2024_09_18_123333_create_students_table.php
+++ database/migrations/2024_09_18_123333_create_students_table.php
@@ -13,8 +13,17 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('adm_no');
+            $table->unsignedBigInteger('form')->nullable();
+            $table->integer('form_sequence_number')->nullable();
+            $table->unsignedBigInteger('stream_id')->nullable();
+            $table->integer('term')->nullable();
             $table->timestamps();
-        });
+        
+            $table->foreign('form')->references('id')->on('class_forms')->onDelete('cascade');
+            $table->foreign('stream_id')->references('id')->on('streams')->onDelete('cascade');
+        });        
     }
 
     /**
